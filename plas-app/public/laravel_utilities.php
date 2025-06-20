4584ada7cce6c14dccf133d6f0d9744b1d374fdc
benzino98 <bigasamaben33@gmail.com>
2025-06-20 14:01:29 +0100

    Update Allowed IPs in Security Scripts for Enhanced Access Control
    - Added '98.97.79.54' to the list of allowed IPs in 'check_logs.php', 'create_admin.php', 'fix_logs.php', and 'laravel_utilities.php' to improve access control for administrative functionalities.
    - This change ensures that the new IP address is recognized for authorized operations, enhancing security and accessibility for users.
    
    These updates contribute to a more secure and manageable logging and administrative environment.

--- plas-app/public/laravel_utilities.php
+++ plas-app/public/laravel_utilities.php
@@ -16,6 +16,7 @@
 $allowed_ips = [
     // Add your IP address here
     '102.91.104.42',
+    '98.97.79.54',
     '102.91.103.139',
     '135.129.124.105',
     '127.0.0.1', // localhost for development
